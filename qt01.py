01e02c9f910265b074db1714f511e11c8603238b
wyang <563033455@qq.com>
2019-11-15 14:39:19 +0800

    添加样式

--- qt01.py
+++ qt01.py
@@ -1,15 +1,5 @@
 # -*- coding: utf-8 -*-
 
-"""
-PyQt5 tutorial
-
-In this example, we determine the event sender
-object.
-
-author: py40.com
-last edited: 2017年3月
-"""
-
 # 导入需要的包和模块
 import sys
 from PyQt5.QtWidgets import qApp, QMainWindow, QPushButton, QApplication, QWidget, QLabel
@@ -19,8 +9,26 @@ from Init import Window
 app = QApplication(sys.argv)
 # # 操作控件
 
+# ## 读取样式
+with open('styles.qss') as style:
+    qApp.setStyleSheet(style.read())
+
 # ## 创建控件
 window = Window()
+
+label = QLabel(window)
+label.setText('测试')
+
+button = QPushButton(window)
+button.setText('启动')
+button.setProperty('attr', 'open')
+button.move(100, 100)
+
+button = QPushButton(window)
+button.setText('关闭')
+button.setProperty('attr', 'close')
+button.move(200, 100)
+
 # 展示控件
 window.show()
 # # 应用程序的执行，进入到消息循环
