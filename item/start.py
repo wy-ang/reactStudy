89c4823baccbe5027fb0226dd45c8f8f9bff5a39
wy-ang <wyang1117@msn.com>
2019-11-24 16:43:11 +0800

    添加多线程开关

--- item/start.py
+++ item/start.py
@@ -4,48 +4,51 @@ from item.UI.reactStudy import Ui_ReactStudy
 from PyQt5.QtCore import QThread, pyqtSignal
 import os, subprocess
 
+# 获取当前工作目录
 cwd = os.getcwd()
+# 项目路径
 pathList = []
-cmdList = []
+# 读取配置文件
+file = open(cwd + '\config.txt', mode='r')
+try:
+    while True:
+        line = file.readline()
+        if line:
+            if 'ItemPath ' in line:
+                item = line.split('ItemPath ')
+                pathList.append(item[1])
+        else:
+            break
+finally:
+    file.close()
+pathStr = 'cd /d ' + eval("".join(pathList)) + ' && npm start -d';
 
 class startAddItemthread(QThread):
     # 定义线程需要用到的信号
     startItem = pyqtSignal()
     def __init__(self):
-        super().__init__()
-
+        super(startAddItemthread, self).__init__()
+        self.isStart = True
     '''
     @description: 执行线程相关代码
     '''
     def run(self):
         # 执行bash命令
-        file = open(cwd + '\config.txt', mode='r')
-        try:
-            while True:
-                line = file.readline()
-                if line:
-                    if 'ItemPath ' in line:
-                        item = line.split('ItemPath ')
-                        pathList.append(item[1])
-                else:
-                    break
-        finally:
-            file.close()
-        pathStr = 'cd /d ' + eval("".join(pathList)) + ' && npm start';
-
-        startPopen = subprocess.Popen(pathStr, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
-        while True:
-            data = startPopen.stdout.readline()
-            if data != b'':
-                print(data)
-
-        startPopen.stdout.close()
-        startPopen.wait()
-
-        # cmd = os.system(pathStr)
-        # print(cmd)
-        # 发送信号
-        self.startItem.emit()
+        if self.isStart:
+            print(pathStr)
+            startPopen = subprocess.Popen(pathStr, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
+            while self.isStart:
+                data = startPopen.stdout.readline()
+                if data != b'':
+                    print(data)
+            startPopen.stdout.close()
+            startPopen.wait()
+            #
+            # cmd = os.system(pathStr)
+            # print(cmd)
+            # 发送信号
+            self.startItem.emit()
+            self.sleep(1)
 
 class Window(QWidget, Ui_ReactStudy):
     # 判断启动按钮是否可用 #
@@ -54,6 +57,7 @@ class Window(QWidget, Ui_ReactStudy):
         self.setupUi(self)
         # 绑定按钮槽函数
         self.startBtn.clicked.connect(self.clickStartBtn)
+        self.stopBtn.clicked.connect(self.clickStopBtn)
         # 声明线程实例
         self.startItemThread = startAddItemthread()
 
@@ -62,12 +66,15 @@ class Window(QWidget, Ui_ReactStudy):
     '''
     def clickStartBtn(self):
         # 启动线程
+        self.startItemThread.isStart = True
         self.startItemThread.start()
+        self.stateColor.setStyleSheet('background-color: rgb(0, 170, 0)')
         self.startBtn.setEnabled(False)
         self.msgView.append('正在启动进程...')
-        self.stateColor.setStyleSheet('background-color: rgb(0, 170, 0)')
+        print(self.startItemThread.isFinished())
 
     def clickStopBtn(self):
+        self.startItemThread.isStart = False
         stopPopen = subprocess.call('taskkill /f /im node.exe', shell=True)
         if (stopPopen == 0):
             self.msgView.append('进程已停止...')
